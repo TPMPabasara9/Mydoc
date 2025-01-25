7a7722e0f54705e1c17c61927747af4ad34fd0e7
unknown <madushapabasara259@gmail.com>
2025-01-25 22:36:14 +0530

    inital commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './index.css';
+import { Route,Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Appointmnet from './Pages/Appointmnet';
+import Contact from './Pages/Contact';
+import Profile from './Pages/Profile';
+import Doctors from './Pages/Doctors';
+import About from './Pages/About';
+import MyAppointments from './Pages/MyAppointments';
+import Navbar from './Components/Navbar';
+
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%] ">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/doctors/:speciality" element={<Doctors/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/my-profile" element={<Profile/>} />
+        <Route path="/my-appointments" element={<MyAppointments/>} />
+        <Route path="/appointment/:doctorId" element={<Appointmnet/>} />
+      </Routes>
+    </div>
+  
+  )
+}
+
+export default App;
---
+++ frontend/src/Pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About=()=> {
+  return (
+    <div className='text-black'>
+        About
+    </div>
+  )
+}
+export default About;
