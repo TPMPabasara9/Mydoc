b3714a4ad98c8594453b544bbb4f8a9f9e3aa3c3
TPMPabasara9 <madushapabasara259@gmail.com>
2025-07-27 16:23:42 +0530

    add backend route to get dashboard data

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -10,7 +10,12 @@ const AdminContextProvider = (props) => {
     const [aToken,setAToken] =useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken'):'');
     const [doctors,setDoctors] = useState([])
     const [appointments, setAppointments] = useState([]);
+    const [dashData, setDashData] = useState(false);
+
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+
+
     const getAllDoctors = async () =>{
         try {
             
@@ -56,12 +61,26 @@ const AdminContextProvider = (props) => {
             toast.error(error.message);
         }
     };
+    const getDashboardData = async () => {
+        try {
+            const { data } = await axios.get(backendUrl + '/api/admin/dashboard', { headers: { aToken } });
+            if (data.success) {
+                setDashData(data.dashData);
+                console.log("Dashboard data fetched successfully:", data.dashData);
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
 
     const value = {
         aToken,setAToken,
         backendUrl,
         getAllDoctors,
         doctors,changeAvailability,getAllAppointments,appointments
+        ,getDashboardData,dashData
     }
 
     return (
