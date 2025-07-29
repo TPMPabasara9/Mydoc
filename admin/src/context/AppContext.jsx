87e59b215cc31837b9f56b3b26c7f836881c6195
TPMPabasara9 <madushapabasara259@gmail.com>
2025-07-29 19:52:41 +0530

    modify the app context of the admin and doctor

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -5,9 +5,30 @@ export const AppContext = createContext();
 
 const AppContextProvider = ({children }) => {
 
+    const currencySymbol = 'LKR'; // Assuming a default currency symbol
+
+const calculateAge = (dob) => {
+    const today = new Date();
+    const birthDate = new Date(dob);
+
+    let age = today.getFullYear() - birthDate.getFullYear();
+    return age;
+}
+
+const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split('_')
+    return dateArray[0]+" "+ months[Number(dateArray[1])] + " " + dateArray[2];
+}
     const value = {
+        calculateAge,
+        slotDateFormat,
+        currencySymbol
 
     }
+
+
     return (
         <AppContext.Provider value={value}>
             {children}
