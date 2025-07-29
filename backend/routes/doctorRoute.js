ebb7d8ed8726db1af6203b8b0c2c53c0402bc10d
TPMPabasara9 <madushapabasara259@gmail.com>
2025-07-29 20:47:20 +0530

    doctor  routes without doctors dash board route

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,9 +1,15 @@
 import express from 'express'
-import { doctorList } from '../controllers/doctorController.js'
+import { appointmentCancel, appointmentComplete, doctorProfile, getAppointments,loginDoctor} from '../controllers/doctorController.js';
+import authDoctor from '../middleware/authDoctor.js';
+// import authDoctor from '../middleware/authDoctor.js';
 
 const doctorRouter = express.Router()
 
-doctorRouter.get('/list',doctorList);
 
+doctorRouter.post('/login',loginDoctor);
+doctorRouter.get('/doctor-appointments',authDoctor,getAppointments);
+doctorRouter.get('/doctor-profile',authDoctor,doctorProfile); // Assuming this is for getting doctor's profile, adjust as needed
+doctorRouter.post('/complete-appointment',authDoctor,appointmentComplete);
+doctorRouter.post('/cancel-appointment',authDoctor,appointmentCancel); // Assuming this is for completing an appointment, adjust as needed
 
 export default doctorRouter;
